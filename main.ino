fb926ef87f4a13c100e26a5e5960dc864deffcc2
hippolyte.valicon <hippolyte.valicon@yahoo.fr>
2018-03-12 18:07:36 +0100

    tp fini encore une fois

--- main.ino
+++ main.ino
@@ -6,9 +6,18 @@
 // the setup function runs once when you press reset or power the board
 void setup()
 {
+    pinMode(13, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
+    static int potPosition;
+
+    potPosition = analogRead(A0);
+
+    digitalWrite(13, HIGH);
+    delay(potPosition);
+    digitalWrite(13, LOW);
+    delay(potPosition);
 }
